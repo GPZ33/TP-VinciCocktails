0425d7300783e22d52bb82457dd15d1428948cc2
gpz33 <remi.pedeflous@gmail.com>
2023-12-14 13:38:20 +0100

    feat: Added feat search bar in header
    Ajout de la fonctionnalité de recherche pour la barre de recherche du header avec auto completation. Problème relevé : l'auto completation n'affiche que les 25 premiers verres à cause de l'api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,46 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
 
 const Header = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+
+  //Attention, avec l'API qui limite la requête fetch à 25 resultats, les resultats de la recherche ne marche que pour ces 25 verres
+
+  const [drinks, setDrinks] = useState(null);
+  const [inputText, setInputText] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+
+    (async () => {
+      const drinksResponse = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s");
+      const drinksResponseData = await drinksResponse.json();
+
+      setDrinks(drinksResponseData.drinks);
+    })();
+  }, []);
+
+  const inputHandler = (e) => {
+    const lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+
+    // Filtrer les boissons correspondant à la recherche
+    const filteredDrink = drinks.filter((el) => {
+      return el.strDrink.toLowerCase().includes(lowerCase);
+    });
+
+    // Mettre à jour les résultats de la recherche
+    setSearchResults(filteredDrink);
+  };
+
+  console.log(inputText);
+  console.log(searchResults, "mais t'es ou mais t'es pas la");
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
       <Container fluid>
         <Navbar.Brand>VINCI Cocktails</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -19,7 +51,7 @@ const Header = () => {
             navbarScroll
           >
             <Nav.Link><Link to="/">Home</Link></Nav.Link>
-            
+
             <Nav.Link><Link to="/drinks">Nos verres</Link></Nav.Link>
             <Nav.Link><Link to="/cocktails">Nos cocktails</Link></Nav.Link>
             <Nav.Link><Link to="/categories">Nos categories</Link></Nav.Link>
@@ -38,18 +70,27 @@ const Header = () => {
               Link
             </Nav.Link>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex position-relative">
             <Form.Control
               type="search"
               placeholder="Search"
-              className="me-2"
+              className="me-2 flex-grow-1"
               aria-label="Search"
+              onChange={inputHandler}
             />
-            <Button variant="outline-success">Search</Button>
+            {searchResults.length > 0 && (
+              <div style={{ position: 'absolute', top: '100%', left: 0, width: '100%', background: '#fff', zIndex: 100 }}>
+                <ul style={{ listStyle: 'none', padding: 0 }}>
+                  {searchResults.map((drink) => (
+                    <li key={drink.idDrink}><Link to={`/drink/details/${drink.idDrink}`}>{drink.strDrink}</Link></li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 }
 export default Header;
